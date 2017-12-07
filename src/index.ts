0dd87b9c074d932fab6830b406cc7e02109bad12
Marco <marcohavermans+github@hotmail.nl>
2017-12-07 12:34:07 +0100

    Fixed auth on cors

--- src/index.ts
+++ src/index.ts
@@ -34,9 +34,13 @@ app.use((req, res, next) => {
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
 
-    next();
+    if ('OPTIONS' === req.method) {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 authentication.setup(app);
