31214cc8cc447516ba6eef7887c5dc87e674a0cb
Marco <marcohavermans+github@hotmail.nl>
2017-12-10 19:28:37 +0100

    Changed environment variable names

--- src/config/config.const.ts
+++ src/config/config.const.ts
@@ -6,6 +6,6 @@ export const Config: IConfig = {
     port: (process.env.PORT || config.port) as number,
     mongoDbUri: process.env.MONGODB_URI || config.mongoDbUri,
     allowOrigin: process.env.ALLOW_ORIGIN || config.allowOrigin,
-    secret: process.env.secret || config.secret,
-    jwtExpires: process.env.jwtExpires || config.jwtExpires
+    secret: process.env.SECRET || config.secret,
+    jwtExpires: process.env.JWT_EXPIRES || config.jwtExpires
 };
